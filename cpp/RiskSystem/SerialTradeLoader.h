40d6612de0a2a33888b5451ada6b5f1cc1af18b9
Khaled Darwish <37257033+khaledalielden@users.noreply.github.com>
2025-12-26 18:58:09 +0000

    Comment out FxSerialTradeLoader class in header
    Commented out the FxSerialTradeLoader class definition.

--- cpp/RiskSystem/SerialTradeLoader.h
+++ cpp/RiskSystem/SerialTradeLoader.h
@@ -15,5 +15,12 @@ private:
 public:
     std::vector<std::vector<ITrade*>> loadTrades();
 };
-
+/*
+class FxSerialTradeLoader {
+private:
+    std::vector<FXITradeLoader*> FxgetTradeLoaders();
+public:
+    std::vector<std::vector<FXITrade*>> loadTrades();
+};
+*/
 #endif // SERIALTRADELOADER_H
